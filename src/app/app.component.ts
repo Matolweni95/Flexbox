aae65ffb62660fbf78b452297967e97143d8f56d
masixolematolweni.1@gmail.com <72172920+Matolweni95@users.noreply.github.com>
2022-01-27 13:14:03 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Displays {
+  value: string;
+  viewValue: string;
+}
+
+interface Justify {
+  value: string;
+  viewValue: string;
+}
+
+interface Align {
+  value: string;
+  viewValue: string;
+}
+
+interface Height {
+  value: string;
+  viewValue: string;
+}
+
+interface Direction {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +31,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Flexbox';
+  styles:any;
+  selectedValue!: string;
+  selectedJustify!: string;
+  selectedAlign!: string;
+  selectedHeight!: string;
+  selectedDirection!: string;
+
+  display: Displays[] = [
+    {value: '0', viewValue: 'flex'},
+
+  ];
+
+  justify: Justify[] = [
+    {value: '9', viewValue: 'center'},
+  ];
+
+  align: Align[] = [
+    {value: '5', viewValue: 'center'},
+
+  ];
+
+  height: Height[] = [
+    {value: '8', viewValue: '100%'},
+  ];
+
+  direction: Direction[] = [
+    {value: '1', viewValue: 'row'},
+    {value: '2', viewValue: 'column'},
+    {value: '3', viewValue: 'column-reverse'},
+    {value: '4', viewValue: 'row-reverse'},
+  
+  ];
+
+  try(){
+    var span = (<HTMLInputElement>document.getElementById("blocks")).style.display = this.selectedValue;
+    span = (<HTMLInputElement>document.getElementById("blocks")).style.justifyContent = this.selectedJustify;
+    span = (<HTMLInputElement>document.getElementById("blocks")).style.alignItems = this.selectedAlign;
+    span = (<HTMLInputElement>document.getElementById("blocks")).style.height = this.selectedHeight;
+    span = (<HTMLInputElement>document.getElementById("blocks")).style.flexDirection = this.selectedDirection;
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatSelectModule} from '@angular/material/select'; 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatTooltipModule} from '@angular/material/tooltip'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
